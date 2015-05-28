2eac74168d871bd42388d20f5a9c5bb8afd7d6c1
Cameron Webster <cameron.webster@gmail.com>
2015-05-28 14:37:20 -0600

    add day, start and end methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,21 @@
 class Event < ActiveRecord::Base
+
+  def day
+    date = self.start_time
+    date.strftime("%d/%m/%Y")
+  end
+
+  def start
+    time = self.start_time
+    time.strftime("%H:%M")
+  end
+
+  def end
+    time = self.end_time
+    time.strftime("%H:%M")
+  end
+
+  def track_name
+    self.track.split("-")[1]
+  end
 end
