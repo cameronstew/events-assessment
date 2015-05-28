b7beedd1c3199bbf29d1a6278f2752aa76931de7
Cameron Webster <cameron.webster@gmail.com>
2015-05-28 14:38:15 -0600

    create two actions to map to both days

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,15 @@
 class EventsController < ApplicationController
-  def index
+  before_action :set_event
+
+  def day_one
+  end
+
+  def day_two
+  end
+
+  private
+  def set_event
     @events = Event.all
+    @tracks = @events.map {|event| event.track if event.track != ("Lunch" || "Party")}.uniq.compact
   end
 end
